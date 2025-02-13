e8ac9ec89903064979f9193535310d6cf1aff911
dnavale <dnavale@purdue.edu>
2025-02-13 16:41:52 -0500

    resume page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ResumePage from './components/ResumePage';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <ResumePage />
       <Footer />
     </div>
   );
---
+++ src/components/ResumePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/ResumePage.css';
+import pdfFile from '../assets/resume.pdf';
+
+const ResumePage = () => {
+  return (
+    <div className="resume-page-container">
+      <div className="left-section">
+        <h1 className="resume-title">Resume</h1>
+        <p className="resume-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+      </div>
+
+      <div className="right-section">
+        <object data={pdfFile} type="application/pdf" width="100%" height="600px">
+          <p>Your browser does not support PDFs. <a href={pdfFile}>Download the PDF</a>.</p>
+        </object>
+      </div>
+    </div>
+  );
+};
+
+export default ResumePage;
