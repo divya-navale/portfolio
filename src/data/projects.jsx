56c8eedbffe5739e2e3d6683064d9a7b05c63012
dnavale <dnavale@purdue.edu>
2025-02-20 14:29:10 -0500

    images fix

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-
+import illiad from '../assets/illiad.png';
+import transactify from '../assets/transactify.png';
+import ats from '../assets/ats.png';
+import campusmart from '../assets/campusmart.png';
 const projects = [
     {
-        image: "",
-        title: "LIBRARY ILLIAD USER SYSTEM",
+        image: illiad,
+        title: "LIBRARY ILLIAD DATAHUB",
         description: ""
     },
     {
-        image: "",
+        image: transactify,
         title: "LIBRARY TRANSACTIFY",
         description: ""
     },
     {
-        image: "../assets/ats.png",
+        image: ats,
         title: "ATTENDANCE TRACKING SYSTEM",
         description: "Developing an attendance tracking system for Purdue lecturers using React and Material UI, with Firebase backend to ensure real-time data updates and secure user authentication. Implemented a user dashboard and set up authentication using Firebase Authentication, facilitating a seamless and secure user experience. Leveraging Firebase Realtime Database for dynamic attendance data management, supporting real-time updates and scalability."
     },
     {
-        image: "../assets/campusmart.png",
+        image: campusmart,
         title: "CAMPUS MART",
         description: "Developed a specialized e-commerce platform for Purdue students using React, Node.js, and MongoDB, addressing the limitations of informal WhatsApp marketplaces with structured product listings and enhanced communication. Built secure REST APIs for user authentication and product management, integrating Cloudinary for optimized image storage to support efficient media handling. Crafted an intuitive, responsive UI with Bootstrap and SCSS, providing a user-friendly experience tailored to the needs of the campus community."
     },
@@ -31,11 +33,11 @@ const projects = [
     //     title: "IDENTIFICATION OF NEAR-DUPLICATE DOCUMENTS USING SIMHASH ALGORITHM",
     //     description: "Implemented simhash algorithm which uses map and reduce techniques to detect duplicate documents using Python."
     // },
-    {
-        image: "../assets/bankingSystem.png",
-        title: "BANKING SYSTEM",
-        description: "The Banking system is a Console-based application that uses hashing and CSV files for storing and accessing the data. It has withdrawal, deposit, and transfer of money from one to another account and other banking operations. (C++, CSV files)"
-    }
+    // {
+    //     image: "../assets/bankingSystem.png",
+    //     title: "BANKING SYSTEM",
+    //     description: "The Banking system is a Console-based application that uses hashing and CSV files for storing and accessing the data. It has withdrawal, deposit, and transfer of money from one to another account and other banking operations. (C++, CSV files)"
+    // }
 ];
 
 export default projects;
